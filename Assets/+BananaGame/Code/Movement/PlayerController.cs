38dcc1133a00e25b5d331963bf27b03b076a7300
Lauri <lauri.kalliola97@outlook.com>
2023-02-08 13:45:33 +0200

    Improved PlayerController and included a new Look method for character rotation

--- Assets/+BananaGame/Code/Movement/PlayerController.cs
+++ Assets/+BananaGame/Code/Movement/PlayerController.cs
@@ -9,12 +9,13 @@ namespace BananaSoup
     {
         [Header("Adjustable variables")]
         [SerializeField] private float movementSpeed = 5.0f;
+        [SerializeField] private float turnSpeed = 360.0f;
 
         private PlayerInput playerInput;
-        private InputAction move;
+        private InputAction moveInput;
 
         private Rigidbody rb;
-        private Vector3 movement = Vector3.zero;
+        private Vector3 movementInput = Vector3.zero;
 
         private void Awake()
         {
@@ -34,7 +35,7 @@ namespace BananaSoup
 
         private void OnEnable()
         {
-            move = playerInput.Player.Move;
+            moveInput = playerInput.Player.Move;
             playerInput.Player.Enable();
         }
 
@@ -46,19 +47,38 @@ namespace BananaSoup
         private void FixedUpdate()
         {
             Move(Time.fixedDeltaTime);
+            Look(Time.fixedDeltaTime);
         }
 
-        // Used mainly for debugging at the moment
-        //private void Update()
-        //{
-        //    Debug.Log("The value of 'move' is: " + move.ReadValue<Vector2>());
-        //    Debug.Log("The value of 'movement' is: " + movement);
-        //}
+        // Store the moveInput values to the movementInput Vector3
+        private void Update()
+        {
+            movementInput.Set(moveInput.ReadValue<Vector2>().x, rb.velocity.y, moveInput.ReadValue<Vector2>().y);
+        }
 
         private void Move(float deltaTime)
         {
-            movement = new Vector3((move.ReadValue<Vector2>().x * movementSpeed * deltaTime), rb.velocity.y, (move.ReadValue<Vector2>().y * movementSpeed * deltaTime));
+            Vector3 movement = (transform.forward * movementInput.magnitude) * movementSpeed * deltaTime;
             rb.MovePosition(transform.position + movement);
+        }
+
+        private void Look(float deltaTime)
+        {
+            if (movementInput != Vector3.zero)
+            {
+                var matrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
+
+                var skewedInput = matrix.MultiplyPoint3x4(movementInput);
+
+                var relative = (transform.position + skewedInput) - transform.position;
+                var rot = Quaternion.LookRotation(relative, Vector3.up);
+
+                // Use this for smooth turning
+                transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, turnSpeed * deltaTime);
+
+                // Use this for instant turning
+                //transform.rotation = rot;
+            }
         }
     }
 }
