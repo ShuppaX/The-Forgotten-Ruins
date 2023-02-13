6d428c0677a5361972a99ebb3c86196680633bc1
Tomi Sepänmaa <tomi.sepanmaa@tuni.fi>
2023-02-13 20:06:38 +0200

    Added BaseSceneLoader.prefab to ensure BaseScene is always loaded. Also handles that there won't be two BaseScenes at the same time.
    Also deleted unnecessary GameObjects from EnvironmentTest.scene because of added BaseScene. Added BaseScene.scene and EnvironmentTest.scene to build settings. BaseSceneLoader requires BaseScene.scene in build settings!

---
+++ Assets/+BananaGame/Code/BaseSceneLoader.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using NaughtyAttributes;
+
+namespace BananaSoup
+{
+    public class BaseSceneLoader : MonoBehaviour
+    {
+        [SerializeField, Scene] private int baseScene;
+
+        private void Awake()
+        {
+            LoadBaseScene();
+        }
+
+        /// <summary>
+        /// Go through every loaded Scene in the game. If BaseScene loaded, end the method. Otherwise load BaseScene as Additive.
+        /// BaseScene contains GameObjects that are required in every scene like a camera or player.
+        /// </summary>
+        private void LoadBaseScene()
+        {
+            for ( int i = 0; i < SceneManager.sceneCount; i++ )
+            {
+                if ( SceneManager.GetSceneAt(i).buildIndex == baseScene )
+                {
+                    Debug.Log("Base Scene already loaded. End BaseScene loading.");
+                    return;
+                }
+            }
+
+            SceneManager.LoadSceneAsync(baseScene, LoadSceneMode.Additive);
+        }
+    }
+}
