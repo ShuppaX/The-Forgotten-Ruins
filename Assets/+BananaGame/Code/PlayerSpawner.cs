979664931ede89fd381374d7984c3f58e4a35ffa
Tomi Sepänmaa <tomi.sepanmaa@tuni.fi>
2023-02-14 07:10:28 +0200

    Added PlayerSpawnPoint to spawn the player to a specific location when started. NOTE: Not working atm. because PlayerBase is not in main branch

---
+++ Assets/+BananaGame/Code/PlayerSpawner.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BananaSoup
+{
+    public class PlayerSpawner : MonoBehaviour
+    {
+        private void Start()
+        {
+            // TODO: Enable this when PlayerBase is merged to the main branch.
+            //if ( PlayerBase.Instance != null )
+            //{
+            //    PlayerBase.Instance.gameObject.transform.position = transform.position;
+            //}
+            //else
+            //{
+            //    Debug.LogWarning("Player prefab not found. " + gameObject + " couldn't relocate Player prefab.");
+            //}
+        }
+    }
+}
