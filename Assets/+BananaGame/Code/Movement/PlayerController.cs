1327f1a883ca8c2b57367d2833aceaf5e1b9c1d6
Lauri <lauri.kalliola97@outlook.com>
2023-03-05 03:22:30 +0200

    Tried to implement a calculation for correct movement on slopes (not working at the moment)
    The player can't move at all on slopes (either the force isn't enough or there is some other problem).
    
    The slopes also allow for the player to start flying around, have to work a lot on the movement again.
    
    Implemented a WIP movement speed limiter, which at the moment is a bit screwy as well (falling speed is affected and falling down feels slow).

--- Assets/+BananaGame/Code/Movement/PlayerController.cs
+++ Assets/+BananaGame/Code/Movement/PlayerController.cs
@@ -9,24 +9,48 @@ namespace BananaSoup
     public class PlayerController : MonoBehaviour
     {
         [Header("Movement")]
-        [SerializeField, Tooltip("The amount of force for moving the character.")] private float movementForce = 5.0f;
-        [SerializeField, Tooltip("The amount of drag used while the character is on the ground.")] private float groundDrag = 3.5f;
-        [SerializeField, Tooltip("The amount of drag used while the character is not on the ground.")] private float fallingDrag = 1.0f;
+        [SerializeField, Tooltip("The amount of force for moving the character.")]
+        private float movementForce = 5.0f;
+        [SerializeField, Tooltip("Maximum movementspeed for the character.")]
+        private float maxSpeed = 7.0f;
+        [SerializeField, Tooltip("The amount of drag used while the character is on the ground.")]
+        private float groundDrag = 3.5f;
+        [SerializeField, Tooltip("The amount of drag used while the character is not on the ground.")]
+        private float fallingDrag = 1.0f;
+
+        // This is used to keep the player down on the ground while walking up on slopes.
+        [SerializeField, Tooltip("The downward force on the character on slopes.")]
+        private float downwardSlopeForce = 80.0f;
 
         // This is required for the smooth turning. (In the Look method)
         //[SerializeField, Tooltip("The turning speed of the character while changing direction.")] private float turnSpeed = 360.0f;
 
+        // Variables used to store in script values.
         private Rigidbody rb;
         private Vector3 movementInput = Vector3.zero;
         private Vector3 movementDirection = Vector3.zero;
 
         private bool wasGrounded = false;
-        private float groundCheckOffset = 0.01f;
+        [SerializeField]
+        private float groundCheckOffset = 0.05f;
 
         private float characterWidth = 0;
         private float characterHeight = 0;
 
+        [Header("Slope")]
+        [SerializeField, Tooltip("The maximum angle for a slope the player can walk on.")]
+        private float maxSlopeAngle = 70.0f;
+
+        private RaycastHit slopeHit;
+
         private PlayerBase playerBase;
+
+        // TODO: Decide whether to keep this or not (is there any use for it,
+        // TODO: for example in animations etc.)
+        public bool IsGrounded
+        {
+            get { return GroundCheck(); }
+        }
 
         private void Start()
         {
@@ -49,6 +73,14 @@ namespace BananaSoup
             {
                 Debug.LogError("A PlayerBase couldn't be found on the " + gameObject + "!");
             }
+        }
+
+        private void Update()
+        {
+            SpeedLimiter();
+
+            Debug.Log("Ground check = " + GroundCheck());
+            Debug.Log("RigidBodys velocity = " + rb.velocity);
         }
 
         private void FixedUpdate()
@@ -85,10 +117,48 @@ namespace BananaSoup
         {
             movementDirection += (transform.forward * movementInput.magnitude) * movementForce;
 
-            if ( IsGrounded() )
+            if ( OnSlope() )
+            {
+                rb.AddForce(GetSlopeMoveDirection() * movementForce);
+
+                if ( rb.velocity.y > 0 )
+                {
+                    rb.AddForce(Vector3.down * downwardSlopeForce);
+                }
+            }
+
+            if ( GroundCheck() )
             {
                 rb.AddForce(movementDirection);
                 movementDirection = Vector3.zero;
+            }
+
+            // Turn the RigidBodys
+            rb.useGravity = !OnSlope();
+        }
+
+        private void SpeedLimiter()
+        {
+            // Limit characters movement speed on slopes
+            if ( OnSlope() )
+            {
+                if (rb.velocity.magnitude > maxSpeed )
+                {
+                    rb.velocity = rb.velocity.normalized * maxSpeed;
+                }
+            }
+
+            // Limiting the speed on the ground or air
+            else
+            {
+                Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+                // Limit the velocity if necessary.
+                if (flatVel.magnitude > maxSpeed )
+                {
+                    Vector3 limitedVel = flatVel.normalized * maxSpeed;
+                    rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+                }
             }
         }
 
@@ -99,7 +169,7 @@ namespace BananaSoup
         /// <returns>True if the character was grounded on the previous frame, false if not.</returns>
         private bool SetDrag()
         {
-            if ( IsGrounded() )
+            if ( GroundCheck() )
             {
                 rb.drag = groundDrag;
                 wasGrounded = true;
@@ -142,10 +212,26 @@ namespace BananaSoup
         /// Uses a spherecast to check if the character has something to collide with below it.
         /// </summary>
         /// <returns>True if the spherecast finds something below, false if not.</returns>
-        public bool IsGrounded()
+        private bool GroundCheck()
         {
             RaycastHit rayHit;
             return (Physics.SphereCast(transform.position, characterWidth / 2, Vector3.down, out rayHit, (characterHeight / 2) + groundCheckOffset));
+        }
+
+        private bool OnSlope()
+        {
+            if ( Physics.Raycast(transform.position, Vector3.down, out slopeHit, characterHeight * 0.5f + 0.3f) )
+            {
+                float angle = Vector3.Angle(Vector3.up, slopeHit.normal);
+                return angle < maxSlopeAngle && angle != 0;
+            }
+
+            return false;
+        }
+
+        private Vector3 GetSlopeMoveDirection()
+        {
+            return Vector3.ProjectOnPlane(movementDirection, slopeHit.normal).normalized;
         }
     }
 }
