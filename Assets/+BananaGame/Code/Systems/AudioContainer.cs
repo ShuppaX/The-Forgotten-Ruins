1bc7b31e18b26cacb89c9dcbd0b2885582796069
Samu <sqyber@gmail.com>
2023-03-13 13:22:41 +0200

    Broken audio system

---
+++ Assets/+BananaGame/Code/Systems/AudioContainer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BananaSoup
+{
+
+    [CreateAssetMenu(fileName = "AudioData", menuName = "Data/AudioContainer")]
+    public class AudioContainer : ScriptableObject
+    {
+        [Serializable]
+        public class SoundItem
+        {
+            public SoundEffect Type;
+            public AudioClip Clip;
+        }
+
+        [SerializeField] private SoundItem[] soundEffects;
+
+        /// <summary>
+        /// Returns the audio clip which matches the effect type
+        /// </summary>
+        /// <param name="effectType"></param>
+        /// <returns>Correct clip if it is stored in the soundEffects array. Null otherwise.</returns>
+        public AudioClip GetClipByType(SoundEffect effectType)
+        {
+            foreach (SoundItem soundEffect in soundEffects)
+            {
+                if (soundEffect.Type == effectType)
+                {
+                    return soundEffect.Clip;
+                }
+            }
+
+            return null;
+
+
+        }
+    }
+}
