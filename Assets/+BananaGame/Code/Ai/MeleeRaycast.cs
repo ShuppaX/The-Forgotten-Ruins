460cbc3ba649d5e895c95d93a21e5e7edee40aeb
Tomi Sepänmaa <tomi.sepanmaa@tuni.fi>
2023-03-28 09:42:06 +0300

    Enemies will be stunned when the player throws sand on them

--- Assets/+BananaGame/Code/Ai/MeleeRaycast.cs
+++ Assets/+BananaGame/Code/Ai/MeleeRaycast.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using Random = UnityEngine.Random;
@@ -8,7 +9,7 @@ namespace BananaSoup
     //Requirements
     [RequireComponent(typeof(NavMeshAgent))]
     [RequireComponent(typeof(Rigidbody))]
-    public class MeleeRaycast : MonoBehaviour
+    public class MeleeRaycast : MonoBehaviour, ISandable
     {
         //Definitions
         protected NavMeshAgent _enemy; // assign navmesh agent
@@ -16,7 +17,7 @@ namespace BananaSoup
         protected Vector3 _playerDirection;
 
         //Serialized
-        [Header("Layer masks")] 
+        [Header("Layer masks")]
         [SerializeField] private LayerMask whatIsGround;
         [SerializeField] private LayerMask whatIsPlayer;
 
@@ -27,7 +28,11 @@ namespace BananaSoup
         [Header("Vision")]
         [SerializeField] private float _sightRange;
         [SerializeField] private float _attackRange;
-        
+
+        [Header("Stun")]
+        [SerializeField] private float stunTime = 2.0f;
+        private Coroutine enemyStunnedRoutine;
+
         protected Transform _lookAtTarget;
         protected float _damp = 6f;
 
@@ -36,6 +41,7 @@ namespace BananaSoup
         private readonly float _pauseTime = 1f; //Time to pause after action
 
         //patrol
+        [Space]
         public Vector3 waypoint;
         private bool _waypointSet;
         private float _waypointRange;
@@ -51,7 +57,6 @@ namespace BananaSoup
         private bool _playerInAttackRange;
         private bool _stunned;
 
-
         private void Awake()
         {
             _enemy = GetComponent<NavMeshAgent>();
@@ -66,6 +71,12 @@ namespace BananaSoup
 
         private void Update()
         {
+            // Check is the enemy stunned. If it is, don't continue Update() method.
+            if ( _stunned )
+            {
+                return;
+            }
+
             //Variables
             var position = transform.position;
             _whereIsPlayer = _playerDirection - position;
@@ -75,45 +86,37 @@ namespace BananaSoup
             _playerInAttackRange = Physics.CheckSphere(position, _attackRange, whatIsPlayer);
 
 
-            if (_playerInSightRange)
+            if ( _playerInSightRange )
                 //Smoothed turning towards player
-                if (_angle > 2.5f)
+                if ( _angle > 2.5f )
                 {
                     var rotate = Quaternion.LookRotation(_playerTarget.position - transform.position);
                     transform.rotation = Quaternion.Slerp(transform.rotation, rotate, Time.deltaTime * _damp);
                 }
 
-            if (Time.time < _lastDidSomething + _pauseTime) return;
+            if ( Time.time < _lastDidSomething + _pauseTime ) return;
 
-
-
-            if (!_stunned)
-            {
-                
-                //compressed if statements for clarity
-                if (!_playerInSightRange && !_playerInAttackRange) Patrol();
-                if (_playerInSightRange && !_playerInAttackRange) Chase();
-                if (_playerInSightRange && _playerInAttackRange) Attack();
-                
-            }
-            
+            //compressed if statements for clarity
+            if ( !_playerInSightRange && !_playerInAttackRange ) Patrol();
+            if ( _playerInSightRange && !_playerInAttackRange ) Chase();
+            if ( _playerInSightRange && _playerInAttackRange ) Attack();
         }
 
         private void Patrol()
         {
-            if (!_waypointSet) SearchWaypoint();
+            if ( !_waypointSet ) SearchWaypoint();
 
-            if (_waypointSet) _enemy.SetDestination(waypoint);
+            if ( _waypointSet ) _enemy.SetDestination(waypoint);
 
             var distanceToWayPoint = transform.position - waypoint;
 
             //Waypoint reached
-            if (distanceToWayPoint.magnitude < 1f) _waypointSet = false;
+            if ( distanceToWayPoint.magnitude < 1f ) _waypointSet = false;
 
             _lastDidSomething = Time.time;
         }
 
-        
+
         //Looks for a new waypoint through navmesh
         private void SearchWaypoint()
         {
@@ -124,7 +127,7 @@ namespace BananaSoup
             waypoint = new Vector3(position.x + randomX, position.y,
                 position.z + randomZ);
 
-            if (Physics.Raycast(waypoint, -transform.up, 2f, whatIsGround)) _waypointSet = true;
+            if ( Physics.Raycast(waypoint, -transform.up, 2f, whatIsGround) ) _waypointSet = true;
             _lastDidSomething = Time.time;
         }
 
@@ -138,10 +141,9 @@ namespace BananaSoup
             //Stop enemy movement
             _enemy.SetDestination(transform.position);
 
-
             //transform.LookAt(_playerTarget);
 
-            if (!alreadyAttacked)
+            if ( !alreadyAttacked )
             {
                 //TODO Attack code here
                 Debug.Log("Enemy Swings");
@@ -162,7 +164,7 @@ namespace BananaSoup
         {
             health -= damage;
 
-            if (health <= 0) Invoke(nameof(DestroyEnemy), .5f);
+            if ( health <= 0 ) Invoke(nameof(DestroyEnemy), .5f);
         }
 
         private void DestroyEnemy()
@@ -179,6 +181,38 @@ namespace BananaSoup
             Gizmos.DrawWireSphere(position, _attackRange);
             Gizmos.color = Color.yellow;
             Gizmos.DrawWireSphere(position, _sightRange);
+        }
+
+        public void OnSandAttack()
+        {
+            // Check an try end an old stun Coroutine if there one already running.
+            // The enemy will get strange behaviours if there are multiple stun Coroutines running.
+            TryEndingRunningCoroutine(ref enemyStunnedRoutine);
+
+            enemyStunnedRoutine = StartCoroutine(StunEnemy());
+        }
+
+        private IEnumerator StunEnemy()
+        {
+            _stunned = true;
+            yield return new WaitForSeconds(stunTime);
+            _stunned = false;
+            enemyStunnedRoutine = null;
+        }
+
+        // Ensure that the Coroutine will be stopped and nulled when GameObject get's disabled.
+        private void OnDisable()
+        {
+            TryEndingRunningCoroutine(ref enemyStunnedRoutine);
+        }
+
+        private void TryEndingRunningCoroutine(ref Coroutine routine)
+        {
+            if ( routine != null )
+            {
+                StopCoroutine(routine);
+                routine = null;
+            }
         }
     }
 }
